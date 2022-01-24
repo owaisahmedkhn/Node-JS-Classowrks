f8fa3c3eaa2dc6c1bd22fd4e315819a184bd2036
owaisahmedkhan <owaisahmedkhan@.github.com>
2022-01-24 19:22:50 +0500

    n1

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-const port = 18320
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send("Hello World!, Your Node .mjs Server is running and listening at Port:"+ port)
